36373db63541d0342ef7205081da0d9a582eb37d
FerryJohanes <ferryjohanes2001@gmail.com>
2022-10-16 20:05:44 +0700

    a

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,10 +7,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject grass;
     [SerializeField] GameObject road;
     [SerializeField] int extent;
-    
+    [SerializeField] int frontDistance = 10;
+
     private void Start()
     {
-        for(int z = 0; z < 10; z++)
+        for(int z = 0; z < frontDistance; z++)
         {
             var go = Instantiate(road, new Vector3(0,0,z), Quaternion.identity);
             var tb = go.GetComponent<TerrainBlock>();
